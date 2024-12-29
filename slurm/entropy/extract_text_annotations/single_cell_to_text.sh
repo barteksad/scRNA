995a1d61a22ca9852a01acd66722c517e061e5f3
barteksad <barteksadlej@gmail.com>
2024-12-29 20:40:41 +0100

    feat: refactor for text annotation

---
+++ slurm/entropy/extract_text_annotations/single_cell_to_text.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=sc_rna
+#SBATCH --partition=common
+#SBATCH --qos=4gpu1h
+#SBATCH --time=1:00:00
+#SBATCH --output=slurm_logs/sc_rna-%A.log
+
+source ./env/bin/activate
+
+export HYDRA_FULL_ERROR=1
+
+srun python src/main.py \
+    --config-name single_cell_to_text \
+    exp.temperature=1.0
+
+
+
+
---
+++ src/main.py
@@ -0,0 +1,12 @@
+import hydra
+from omegaconf import DictConfig
+from hydra.utils import call
+import dotenv
+
+@hydra.main(version_base = None, config_path = "../configs")
+def main(config : DictConfig):
+    call(config.exp.run_func, config)
+
+if __name__ == "__main__":
+    dotenv.load_dotenv(override=True)
+    main()
